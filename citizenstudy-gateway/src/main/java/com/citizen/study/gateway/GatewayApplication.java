81a17ee9d6ba79184e1a0be542c013f186482424
maobuji <16770864@qq.com>
2019-03-29 19:57:35 +0800

    修正网关配置，连接nacos

--- citizenstudy-gateway/src/main/java/com/citizen/study/gateway/GatewayApplication.java
+++ citizenstudy-gateway/src/main/java/com/citizen/study/gateway/GatewayApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * Created by zhang on 2019/3/5.
  */
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
