23a094e4bd0c794e0923e4f1c41976e7ae49cf30
16770864@qq.com <16770864@qq.com>
2019-04-03 01:00:41 +0800

    引入auth2但是有冲突

--- citizenstudy-auth/src/main/java/com/citizen/study/auth/AuthApplication.java
+++ citizenstudy-auth/src/main/java/com/citizen/study/auth/AuthApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan("com.citizen.study.auth.mapper")
+@EnableAuthorizationServer
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
